5ba3a4040c22087d25000bbbd52c6515f2f334ad
Krishna <Krishnathombare43@gmail.com>
2025-01-06 23:49:31 +0530

    Configured Database and Added HTML Pages.

--- Project/app.py
+++ Project/app.py
@@ -1,7 +1,51 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
+from dotenv import load_dotenv
 import MySQLdb.cursors
 import re
+import os
+
+load_dotenv('.env')
+load_dotenv('db.env')
+
+app = Flask(__name__)
+
+app.secret_key = os.getenv('SECRET_KEY')
+
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
+
+mysql = MySQL(app)
+
+@app.route('/')
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    msg = ''
+    if request.method == 'POST' and 'username' in  request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password,))
+        account = cursor.fetchone()
+        
+        if(account):
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['username']
+            msg = 'Logged in Successfully!'
+            return render_template('index.html', msg=msg)
+        
+        else:
+            msg = 'Incorrect Username/Password! Try Again.'
+    return render_template('login.html', msg=msg)
+
+if(__name__) == "__main__":
+    app.run(debug=True)
+          
+    
+    
 
 
 
