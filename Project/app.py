8a8f6e75512d1647324d8babbb1c4648de1de7fe
Krishna <Krishnathombare43@gmail.com>
2025-01-07 00:01:34 +0530

    Added Logout Route.

--- Project/app.py
+++ Project/app.py
@@ -41,6 +41,23 @@ def login():
             msg = 'Incorrect Username/Password! Try Again.'
     return render_template('login.html', msg=msg)
 
+@app.route('/logout')
+def logout():
+    session.pop('loggedin', None)
+    session.pop('lid', None)
+    session.pop('username', None)
+    return redirect(url_for('login'))
+
+
+
+
+
+
+
+
+
+
+
 if(__name__) == "__main__":
     app.run(debug=True)
           
