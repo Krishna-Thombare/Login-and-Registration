4beecb04b56cd6b61e8155837bbdbbbf3bf91e70
Krishna <Krishnathombare43@gmail.com>
2025-01-07 11:22:31 +0530

    Added Route for Register.

--- Project/app.py
+++ Project/app.py
@@ -48,6 +48,40 @@ def logout():
     session.pop('username', None)
     return redirect(url_for('login'))
 
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        cursor = mysql.connection.cursor(MySQLdb.cursor.dict(cursor))
+        cursor.execute('SELECT * FROM accounts WHERE username = %s', (username,))
+        account = cursor.fetchone()
+        
+        if (account):
+            msg = 'Account already exists!'
+            
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address !'
+            
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Username must contain only characters and numbers!'
+        
+        elif not username or not password or not email:
+            msg = "Please fill out the form!"
+            
+        else:
+            cursor.execute('INSERT INTO accounts VALUES (None, %s, %s, %s)', (username, password, email,))
+            mysql.connection.commit()
+            msg = 'You have successfully registered!'
+    elif request.method == 'POST':
+        msg = 'Please enter you details first before submitting!'
+        
+    return render_template('register.html', msg=msg)
+        
+        
+
 
 
 
